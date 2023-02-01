16c645b03004045a4404fdab120189417851250f
borgepl <pdeborger@gmail.com>
2023-02-01 16:44:13 +0100

    Eager loading Brands and Types

--- Infrastructure/Data/Repositories/ProductRepository.cs
+++ Infrastructure/Data/Repositories/ProductRepository.cs
@@ -19,12 +19,18 @@ namespace Infrastructure.Data.Repositories
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            return await context.Products.FindAsync(id);
+            return await context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await context.Products.ToListAsync();
+            return await context.Products
+                .Include(p => p.ProductType)
+                .Include(p => p.ProductBrand)
+                .ToListAsync();
         }
 
         public async Task<IReadOnlyList<ProductType>> GetProductTypesAsync()
