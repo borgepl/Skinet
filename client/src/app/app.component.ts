d92bee47c876047bad024dc8ba7c854fc3867ef8
borgepl <pdeborger@gmail.com>
2023-02-06 13:48:39 +0100

    Nav-bar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: any[] = [];
+
+  constructor( private http: HttpClient ) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?Pagesize=50').subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error => console.log(error)
+    });
+  }
+
+
 }
