b3e2ee220e23aed9acb56309e13fb0347d6b3af1
borgepl <pdeborger@gmail.com>
2023-02-09 10:40:31 +0100

    Adding pagination

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment';
 import { Brand } from '../shared/models/brand';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
+import { ShopParams } from '../shared/models/shopParams';
 import { Type } from '../shared/models/type';
 
 @Injectable({
@@ -15,12 +16,14 @@ export class ShopService {
 
   constructor( private http: HttpClient) { }
 
-  getProducts(brandId?: number, typeId?: number, sort?: string) {
+  getProducts(shopParams: ShopParams) {
     let params = new HttpParams();
 
-    if (brandId) params = params.append('brandId', brandId);
-    if (typeId) params = params.append('typeId', typeId);
-    if (sort) params = params.append('sort', sort);
+    if (shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId > 0) params = params.append('typeId', shopParams.typeId);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
   }
