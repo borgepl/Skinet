5e857fc1f1a32e88e9131ee63048c2c2e6f3f074
borgepl <pdeborger@gmail.com>
2023-02-17 14:49:33 +0100

    Redis in API

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -5,6 +5,7 @@ using Infrastructure.Data;
 using Infrastructure.Data.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 namespace API.Extensions
 {
@@ -16,8 +17,14 @@ namespace API.Extensions
         {
             services.AddEndpointsApiExplorer();
 
-            services.AddDbContext<StoreContext>(opt => {
+            services.AddDbContext<StoreContext>(opt => 
+            {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var options = ConfigurationOptions.Parse(config.GetConnectionString("Redis"));
+                return ConnectionMultiplexer.Connect(options);
             });
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
