71dad475a32c23b9734fa2744bb0e38640623ab3
borgepl <pdeborger@gmail.com>
2023-02-21 12:56:26 +0100

    BasketController + Redis

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,37 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseApiController
+    {
+        private readonly IBasketRepository basketRepository;
+        public BasketController( IBasketRepository basketRepository)
+        {
+            this.basketRepository = basketRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+        {
+            var basket = await basketRepository.GetBasketAsync(id);
+
+            return Ok(basket ?? new CustomerBasket(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasket basket)
+        {
+            var UpdatedBasket = await basketRepository.UpdateBasketAsync(basket);
+
+            return Ok(UpdatedBasket);
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasket(string id)
+        {
+             await basketRepository.DeleteBasketAsync(id);
+        }
+    }
+}
