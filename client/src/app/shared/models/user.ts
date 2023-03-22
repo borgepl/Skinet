e3dc2788a0924fcf81041d9425d5378067058956
borgepl <pdeborger@gmail.com>
2023-03-22 11:33:29 +0100

    Account Module in client

---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,16 @@
+export interface User {
+
+  email: string;
+  displayName: string;
+  token: string;
+
+}
+
+export interface Address {
+  firstName: string;
+  lastName: string,
+  street: String;
+  city: string,
+  state: string;
+  zipCode: string;
+}
