2d0292d19a9154d7dd9f0cd4c48a8275c349f793
borgepl <pdeborger@gmail.com>
2023-04-17 12:48:05 +0200

    dotnet ef migrations

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using Core.Entities;
+using Core.Entities.OrderAggregate;
 
 namespace Infrastructure.Data
 {
@@ -14,19 +15,27 @@ namespace Infrastructure.Data
                 context.ProductBrands.AddRange(brands);
             }
 
-             if (!context.ProductTypes.Any())
+            if (!context.ProductTypes.Any())
             {
                 var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
                 var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
                 context.ProductTypes.AddRange(types);
             }
 
-             if (!context.Products.Any())
+            if (!context.Products.Any())
             {
                 var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
                 var products = JsonSerializer.Deserialize<List<Product>>(productsData);
                 context.Products.AddRange(products);
             }
+
+            if (!context.DeliveryMethods.Any())
+            {
+                var deliveryMethodsData = File.ReadAllText("../Infrastructure/Data/SeedData/delivery.json");
+                var deliveryMethods = JsonSerializer.Deserialize<List<DeliveryMethod>>(deliveryMethodsData);
+                context.DeliveryMethods.AddRange(deliveryMethods);
+            }
+           
 
             if (context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
             
