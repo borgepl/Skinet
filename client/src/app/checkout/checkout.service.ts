b8233d845c1664549ea74a93783225babca47455
borgepl <pdeborger@gmail.com>
2023-04-21 10:35:56 +0200

    Delivery Checkout methods

--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { deliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  constructor() { }
+  baseUrl = environment.apiUrl;
+
+  constructor( private http: HttpClient) { }
+
+  getDeliveryMethods() {
+    return this.http.get<deliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map(dm => {
+        return dm.sort((a, b) => b.price - a.price)
+      })
+    )
+  }
 }
