77dce3e04c73638629296912dd6000548658be1e
borgepl <pdeborger@gmail.com>
2023-04-26 09:25:39 +0200

    PaymentService and interface in API

--- API/Extensions/ApplicationServiceExtentions.cs
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -30,6 +30,7 @@ namespace API.Extensions
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IPaymentService, PaymentService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
